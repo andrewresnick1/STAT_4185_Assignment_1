7b3fc4e7a4a0a617dd81d935906b1af7a42565c1
andrewresnick1 <andrew.resnick@uconn.edu>
2024-01-28 17:48:09 -0500

    I decrypted the first message.

---
+++ decrypt_message_one.py
@@ -0,0 +1,52 @@
+cipher = {
+    'a':'v',
+    'b':'h',
+    'c':'r',
+    'd':'j',
+    'e':'t',
+    'f':'x',
+    'g':'s',
+    'h':'a',
+    'i':'e',
+    'j':'f',
+    'k':'b',
+    'l':'n',
+    'm':'o',
+    'n':'i',
+    'o':'g',
+    'p':'l',
+    'q':'m',
+    'r':'z',
+    's':'q',
+    't':'u',
+    'u':'c',
+    'v':'k',
+    'w':'d',
+    'x':'p',
+    'y':'y',
+    'z':'w',
+    ' ': '}',
+    '\n': '^',
+    ',': '5',
+    '!': '[',
+    ':':'-',
+    ')':'*',
+    '.': '%' 
+}
+
+encrypted_file = open("C:\\Users\\Andrew\\STAT_4185_Assignment_1\\encrypted_message_one.txt")
+
+encrypted_message = encrypted_file.readline()
+
+encrypted_file.close()
+
+output=""
+#Write code below
+for char in encrypted_message:
+    #check if char in cipher
+    for key, value in cipher.items():
+        if char == value:
+            output += key
+            break
+
+print(output)
